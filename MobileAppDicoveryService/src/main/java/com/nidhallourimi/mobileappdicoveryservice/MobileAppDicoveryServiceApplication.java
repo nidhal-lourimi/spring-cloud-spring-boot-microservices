f93420b3c66679301180f2ee5fd9669ce5c3c0bf
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-01-15 05:39:03 +0100

    minor change

--- MobileAppDicoveryService/src/main/java/com/nidhallourimi/mobileappdicoveryservice/MobileAppDicoveryServiceApplication.java
+++ MobileAppDicoveryService/src/main/java/com/nidhallourimi/mobileappdicoveryservice/MobileAppDicoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.mobileappdicoveryservice;
+package com.nidhallourimi.mobileappdicoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
