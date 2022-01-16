18e3aafd650fa9d7ee0865ade67b42642f3b4f0f
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-01-16 04:03:20 +0100

    register multiple instance on eureka

--- Account-Management-Microservice/src/main/java/com/nidhallourimi/accountmanagementmicroservice/controller/AccountManagementController.java
+++ Account-Management-Microservice/src/main/java/com/nidhallourimi/accountmanagementmicroservice/controller/AccountManagementController.java
@@ -1,5 +1,7 @@
 package com.nidhallourimi.accountmanagementmicroservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "account")
 public class AccountManagementController {
+    @Autowired
+    private Environment env;
+
     @GetMapping(path = "/status/check")
     public String getStatus(){
-        return "account Management working...";
+        return "account Management working on port "+env.getProperty("local.server.port");
     }
 
 }
