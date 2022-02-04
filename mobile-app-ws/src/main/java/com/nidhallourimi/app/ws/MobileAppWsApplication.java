6f7f464327fc9bbf0d1512049d697021f89203da
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-02-04 15:45:52 +0100

    fixing some issues with spring cloud busrefresh

--- mobile-app-ws/src/main/java/com/nidhallourimi/app/ws/MobileAppWsApplication.java
+++ mobile-app-ws/src/main/java/com/nidhallourimi/app/ws/MobileAppWsApplication.java
@@ -1,20 +1,14 @@
 package com.nidhallourimi.app.ws;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
 public class MobileAppWsApplication {
 
     public static void main(String[] args) {
@@ -23,14 +17,3 @@ public class MobileAppWsApplication {
 
 }
 
-/*
-@Configuration
-public class ReloadLookupEvent implements ApplicationListener<RefreshScopeRefreshedEvent> {
-    @Autowired
-    private CacheService cacheService;
-
-    @Override
-    public void onApplicationEvent(RefreshScopeRefreshedEvent event) {
-        cacheService.refreshLookUp();
-    }
-}*/
